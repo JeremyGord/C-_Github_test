3fc0c66201977857ddaf0ddfb0687861fcd5050f
Jeremy Gordon <jerrgord@ut.utm.edu>
2021-11-11 17:57:27 -0600

    Second commit: adding project files.

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+//Jeremy Gordon
+//11-11-2021
+//Main.cpp
+//Simple hello world program to test github pushing and making commits
+
+#include <iostream>
+using namespace std;
+int main () {
+  cout << "Hello world! /n"  << endl;
+  return 0;
+}
